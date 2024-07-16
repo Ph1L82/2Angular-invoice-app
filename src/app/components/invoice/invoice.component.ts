50ef0acf4cdbc4cc28292d7d5d22d1c1a74af84d
Eduardo 'Ph1L' Rodríguez <Ph1L.4ns3lm0@gmail.com>
2024-07-16 12:46:11 -0400

    invoice service + invoice view

--- src/app/components/invoice/invoice.component.ts
+++ src/app/components/invoice/invoice.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { InvoiceService } from '../../services/invoice.service';
+import { Invoice } from '../../models/invoice';
 
 @Component({
   selector: 'app-invoice',
   standalone: true,
   imports: [],
   templateUrl: './invoice.component.html',
-  styleUrl: './invoice.component.css'
+  styleUrl: './invoice.component.css',
 })
-export class InvoiceComponent {
-
+export class InvoiceComponent implements OnInit {
+  public invoice!: Invoice;
+  constructor(private service: InvoiceService) {}
+  ngOnInit(): void {
+    this.invoice = this.service.getInvoice();
+  }
 }
